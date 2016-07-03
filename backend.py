3a70afcf38cb70c998a6ca14317e5c7505eb95ae
George Hotz <geohot@gmail.com>
2016-07-03 15:08:38 -0700

    downloader

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask, request, send_from_directory, Response
 import random, os, base64, json
+import sys
 
 from db import conn
 import base64
@@ -22,5 +23,19 @@ def root():
   return ''.join(ret)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  lst = []
+  if len(sys.argv) > 1:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM images")
+    i = 0
+    for name, data, track in cur.fetchall():
+      print "downloading", name
+      lst.append({"name": name, "data": data, "track": track})
+      i += 1
+    print "got", i
+    st = json.dumps(lst)
+    open("dump.json", "w").write(st)
+  else:
+    #app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
 
